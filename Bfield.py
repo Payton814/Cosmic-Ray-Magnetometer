f8ba2db8d9918273fd5e1043b73584247136c98b
Payton Linton <lintonpayton99@gmail.com>
2026-02-06 09:48:36 -0500

    earths magnetic field dipole

--- Bfield.py
+++ Bfield.py
@@ -1,37 +1,11 @@
-import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
-import pandas as pd
+
+def Bfield_Earth_dipole(r, latitude, longitude, R):
+    B_r = 2*(R/r)**3 * (-31543*np.cos(latitude*np.pi/180) + np.sin(latitude*np.pi/180)*(2298*np.cos(longitude*np.pi/180) + 5922*np.sin(longitude*np.pi/180)))
+    B_theta = -(R/r)**3 * (31543*np.sin(latitude*np.pi/180) + np.cos(latitude*np.pi/180)*(2298*np.cos(longitude*np.pi/180) + 5922*np.sin(longitude*np.pi/180)))
+    B_phi = (R/r)**3 * (2298*np.sin(longitude*np.pi/180) - 5922*np.cos(longitude*np.pi/180))
+    return np.array([B_r, B_theta, B_phi]).transpose()
 
 
-## I here by define the coordinate system such that z goes through the north pole.
-## Positive x goes through longitude = 0
-
-longitude = np.linspace(0, 2*np.pi, 360) ## on a map these are the vertical lines. So changing line moves east west
-latitude = np.linspace(0, np.pi, 180) ## On a map this is the horizontal lines. So to move lines you move north south
-
-def Bfield(latitude, longitude):
-    B_r = 0
-    B_theta = 0
-    B_phi = latitude/(2*np.pi)
-    B = B_phi
-    return B
-
-X = np.zeros((len(latitude), len(longitude)))
-for i in range(len(latitude)):
-    for ii in range(len(longitude)):
-        X[i][ii] = Bfield(latitude[i], longitude[ii])
-
-xlabel = np.linspace(0, 2*np.pi, 10)
-ylabel = np.linspace(0, np.pi, 5)
-
-df = pd.DataFrame(X)
-
-df.to_csv('linear_LUT.csv')
-
-plt.imshow(X, origin = 'lower')
-plt.xticks(xlabel)
-plt.colorbar()
-plt.show()
 
 
